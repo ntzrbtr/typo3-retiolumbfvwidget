4dfc662998829acefb75d02ce5c2a6fdce39f296
Thomas Off <retiolum@googlemail.com>
2012-04-27 15:41:18 +0200

    Update extension icon.

--- ext_tables.php
+++ ext_tables.php
@@ -17,4 +17,9 @@ Tx_Extbase_Utility_Extension::registerPlugin(
 $pluginSignature = strtolower($_EXTKEY . '_Bfvwidget');
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform,recursive';
 t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_widget.xml');
+
+// Define icon for the wizard.
+if (TYPO3_MODE == 'BE') {
+	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses'][$pluginSignature . '_wizicon'] = t3lib_extMgm::extPath($_EXTKEY) . 'Resources/Private/PHP/class.' . $_EXTKEY . '_wizicon.php';
+}
 ?>
