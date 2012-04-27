a3d818e31475caa795757f712ae49538b168d5e2
Thomas Off <retiolum@googlemail.com>
2012-04-27 15:27:52 +0200

    Change formatting.

--- ext_tables.php
+++ ext_tables.php
@@ -6,12 +6,14 @@ if (!defined('TYPO3_MODE')) {
 // Add static TypoScript.
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'BFV widget');
 
-// Register plugin and related FlexForm.
+// Register plugin.
 Tx_Extbase_Utility_Extension::registerPlugin(
 	$_EXTKEY,
 	'Bfvwidget',
 	'BFV widget'
 );
+
+// Register FlexForm for plugin.
 $pluginSignature = strtolower($_EXTKEY . '_Bfvwidget');
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform,recursive';
 t3lib_extMgm::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_widget.xml');
