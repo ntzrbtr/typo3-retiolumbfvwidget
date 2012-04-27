e8b93fdf4cdbec987bc03f407fb53a2f73e1b80a
Thomas Off <retiolum@googlemail.com>
2012-04-27 12:47:59 +0200

    Fix formatting and add some comments.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,17 +3,14 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+// Define plugin.
 Tx_Extbase_Utility_Extension::configurePlugin(
 	$_EXTKEY,
 	'Bfvwidget',
 	array(
 		'Widget' => 'index',
-		
 	),
-	// non-cacheable actions
 	array(
-		
 	)
 );
-
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -3,12 +3,13 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
+// Add static TypoScript.
+t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'BFV widget');
+
+// Register plugin.
 Tx_Extbase_Utility_Extension::registerPlugin(
 	$_EXTKEY,
 	'Bfvwidget',
 	'BFV widget'
 );
-
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'BFV widget');
-
 ?>
