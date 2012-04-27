c2d6d72d5a4eba03f56afb832c9d51934ca1dff4
Thomas Off <retiolum@googlemail.com>
2012-04-27 13:07:06 +0200

    Register a FlexForm to be able to make some settings.

--- ext_tables.php
+++ ext_tables.php
@@ -6,10 +6,11 @@ if (!defined('TYPO3_MODE')) {
 // Add static TypoScript.
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'BFV widget');
 
-// Register plugin.
+// Register plugin and related FlexForm.
 Tx_Extbase_Utility_Extension::registerPlugin(
 	$_EXTKEY,
 	'Bfvwidget',
 	'BFV widget'
 );
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_Bfvwidget', 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_widget.xml');
 ?>
