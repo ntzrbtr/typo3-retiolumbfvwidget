72b44effb26e1358d6da3dff1c1eb814cbec9441
Thomas Off <retiolum@googlemail.com>
2012-04-27 22:07:59 +0200

    Static TypoScript is not needed for this extension.

--- ext_tables.php
+++ ext_tables.php
@@ -3,9 +3,6 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-// Add static TypoScript.
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xml:extension_title');
-
 // Register plugin.
 Tx_Extbase_Utility_Extension::registerPlugin(
 	$_EXTKEY,
