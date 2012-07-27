70c66f39e936965d0435e4a5a1fa6f4658eb2b06
Thomas Off <retiolum@googlemail.com>
2012-07-27 10:55:23 +0200

    Place script parts in separate lines to improve readability of code.

--- Classes/Controller/WidgetController.php
+++ Classes/Controller/WidgetController.php
@@ -30,37 +30,38 @@
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
 class Tx_Retiolumbfvwidget_Controller_WidgetController extends Tx_Extbase_MVC_Controller_ActionController {
-	
+
 	/**
 	 * Display the BFV widget.
-	 * 
-	 * @return void 
+	 *
+	 * @return void
 	 */
 	public function indexAction() {
 		// Create an id based on the settings.
 		$widgetId = md5(serialize($this->settings) . uniqid());
 		$this->view->assign('widgetId', $widgetId);
-		
+
 		// Add required JavaScript.
 		$pageRenderer = $GLOBALS['TSFE']->getPageRenderer();
 		$pageRenderer->addJsFile('http://ergebnisse.bfv.de/javascript/widgets/tmwrWidgetFunctions.js');
 		$pageRenderer->addJsInlineCode('bfvwidget_' . $widgetId, $this->getJavaScript($widgetId));
 	}
-	
+
 	/**
 	 * Get the JavaScript code needed to initialize the widget.
-	 * 
+	 *
 	 * @param string $widgetId Id of widget to initialize
 	 * @return string
 	 */
 	public function getJavaScript($widgetId) {
-		$javaScript = "var bfvwidget_{$widgetId} = new BFVLigaWidget();";
-		$javaScript .= "bfvwidget_{$widgetId}.setzeLigaNr('{$this->settings['league']}');";
+        $javaScriptLines = array();
+		$javaScriptLines[] = "var bfvwidget_{$widgetId} = new BFVLigaWidget();";
+		$javaScriptLines[] = "bfvwidget_{$widgetId}.setzeLigaNr('{$this->settings['league']}');";
 		if ($this->settings['team'] !== '') {
-			$javaScript .= "bfvwidget_{$widgetId}.setzeVereinNr('{$this->settings['team']}');";
+			$javaScriptLines[] = "bfvwidget_{$widgetId}.setzeVereinNr('{$this->settings['team']}');";
 		}
-		$javaScript .= "bfvwidget_{$widgetId}.{$this->settings['tab']}('bfvwidget_{$widgetId}');";
-		return $javaScript;
+		$javaScriptLines[] = "bfvwidget_{$widgetId}.{$this->settings['tab']}('bfvwidget_{$widgetId}');";
+		return implode("\n", $javaScriptLines);
 	}
 
 }
