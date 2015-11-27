d12961129c249f75bc4d44b357ab7660b2cf1504
Thomas Off <retiolum@googlemail.com>
2015-11-27 08:17:33 +0100

    Add namespace to plugin definition; replace the wizard icon hook class by simply adding some page TS.

--- ext_tables.php
+++ ext_tables.php
@@ -5,17 +5,30 @@ if (!defined('TYPO3_MODE')) {
 
 // Register plugin.
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
+	'Retiolum.' . $_EXTKEY,
 	'Bfvwidget',
 	'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xml:bfvwidget_title'
 );
 
-// Register FlexForm for plugin.
+// Define the plugin signature.
 $pluginSignature = strtolower($_EXTKEY . '_Bfvwidget');
+
+// Register FlexForm for plugin.
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_widget.xml');
 
 // Define icon for the wizard.
-if (TYPO3_MODE == 'BE') {
-	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses'][$_EXTKEY . '_wizicon'] = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Resources/Private/PHP/class.' . $_EXTKEY . '_wizicon.php';
+$extRelPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+<<<EOT
+mod.wizards.newContentElement.wizardItems.plugins.elements.{$pluginSignature} {
+	title = LLL:EXT:retiolumbfvwidget/Resources/Private/Language/locallang_be.xlf:bfvwidget_title
+	description = LLL:EXT:retiolumbfvwidget/Resources/Private/Language/locallang_be.xlf:bfvwidget_description
+	icon = {$extRelPath}/Resources/Public/Icons/bfvwidget.gif
+	tt_content_defValues {
+		CType = list
+		list_type = {$pluginSignature}
+	}
 }
+EOT
+);
