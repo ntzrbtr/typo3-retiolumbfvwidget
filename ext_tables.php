d188266dfaa02e875549f87bb0a9c93abf0ef288
Thomas Off <retiolum@googlemail.com>
2015-12-02 22:04:11 +0100

    Move PageTS code to separate file.

--- ext_tables.php
+++ ext_tables.php
@@ -10,25 +10,10 @@ if (!defined('TYPO3_MODE')) {
 	'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xlf:bfvwidget_title'
 );
 
-// Define the plugin signature.
-$pluginSignature = strtolower($_EXTKEY . '_Bfvwidget');
-
 // Register FlexForm for plugin.
+$pluginSignature = strtolower($_EXTKEY . '_Bfvwidget');
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_widget.xml');
 
 // Define icon for the wizard.
-$extRelPath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-<<<EOT
-mod.wizards.newContentElement.wizardItems.plugins.elements.{$pluginSignature} {
-	title = LLL:EXT:retiolumbfvwidget/Resources/Private/Language/locallang_be.xlf:bfvwidget_title
-	description = LLL:EXT:retiolumbfvwidget/Resources/Private/Language/locallang_be.xlf:bfvwidget_description
-	icon = {$extRelPath}/Resources/Public/Icons/bfvwidget.gif
-	tt_content_defValues {
-		CType = list
-		list_type = {$pluginSignature}
-	}
-}
-EOT
-);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:retiolumbfvwidget/Configuration/TSconfig/PageTS.txt">');
