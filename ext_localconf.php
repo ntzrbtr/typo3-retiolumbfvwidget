fb0224eb154f22717d712b2c570620a03b743d2c
Thomas Off <retiolum@googlemail.com>
2016-03-03 23:06:06 +0100

    Use a boot function as it seems to be good practice and state-of-the-art right now.

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,13 +3,17 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-// Define plugin.
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Retiolum.' . $_EXTKEY,
-	'Bfvwidget',
-	array(
-		'Widget' => 'index',
-	),
-	array(
-	)
-);
+$boot = function($_EXTKEY) {
+	// Define plugin.
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+		'Retiolum.' . $_EXTKEY,
+		'Bfvwidget',
+		array(
+			'Widget' => 'index',
+		),
+		array(
+		)
+	);
+};
+$boot($_EXTKEY);
+unset($boot);
--- ext_tables.php
+++ ext_tables.php
@@ -3,20 +3,24 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-// Register plugin.
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	'Retiolum.' . $_EXTKEY,
-	'Bfvwidget',
-	'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xlf:bfvwidget_title'
-);
+$boot = function($_EXTKEY) {
+	// Register plugin.
+	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+		'Retiolum.' . $_EXTKEY,
+		'Bfvwidget',
+		'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_be.xlf:bfvwidget_title'
+	);
 
-// Register FlexForm for plugin.
-$pluginSignature = strtolower($_EXTKEY . '_Bfvwidget');
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_widget.xml');
+	// Register FlexForm for plugin.
+	$pluginSignature = strtolower($_EXTKEY . '_Bfvwidget');
+	$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/flexform_widget.xml');
 
-// Define icon for the wizard.
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:retiolumbfvwidget/Configuration/TSconfig/PageTS.txt">');
+	// Define icon for the wizard.
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSconfig/PageTS.txt">');
 
-// Add static TypoScript template.
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'EXT:' . $_EXTKEY . '/Configuration/TypoScript/', $_EXTKEY);
+	// Add static TypoScript template.
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'EXT:' . $_EXTKEY . '/Configuration/TypoScript/', $_EXTKEY);
+};
+$boot($_EXTKEY);
+unset($boot);
